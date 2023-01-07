c8b788f71b26f6c3df73db902fa617e742f8a9ad
bmudunur <bmudunur@INLEN8520075791.corp.capgemini.com>
2023-01-07 12:49:04 +0530

    first project push

---
+++ Inteface/src/com/capg/Source.java
@@ -0,0 +1,16 @@
+package com.capg;
+
+public abstract class Source {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ICalculator obj=new StandardCalc();
+		obj.addition();
+		ICalculator calc= ()->System.out.println(5+7);
+		calc.addition();
+		
+		
+
+	}
+
+}
---
+++ Inteface/src/com/capg/StandardCalc.java
@@ -0,0 +1,12 @@
+package com.capg;
+
+public class StandardCalc implements ICalculator{
+
+	@Override
+	public void addition() {
+		// TODO Auto-generated method stub
+		System.out.println(3+6);
+		
+	}
+
+}
